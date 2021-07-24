8453d193a6521bfd0d71601e60ab0fce27c64730
kosetomonori <kose.tomonori@it-sp.co.jp>
2021-07-24 15:39:45 +0900

    test

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ server "aqua-flex-cust.net", user: "deploy", roles: %w{web db app}
 # ssh接続設定
 set :ssh_options, {
   user: fetch(:user),
-  port: ,
+  #port: ポート番号,
   keys: %w(C:\Users\kose.tomonori\.ssh\id_rsa),
   forward_agent: true,
   auth_methods: %w[publickey]
